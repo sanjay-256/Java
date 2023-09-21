82ed5c5033700fab476102994334bfa5b97ff095
sanjay-256 <127410869+sanjay-256@users.noreply.github.com>
2023-09-21 11:06:41 +0530

    hhtest

---
+++ practise1.java
@@ -0,0 +1,25 @@
+public class practise1 {
+    public static void main(String[] args) {
+        int numrows=3,numcols=3;
+        int [][] matrix=new int[numrows][numcols];
+        int value=1;
+        for(int row=0;row<numrows;row++)
+        {
+            for(int col=0;col<numcols;col++)
+            {
+                matrix[row][col]=value;
+                value++;
+            }
+        }
+        //print
+         for(int row=0;row<numrows;row++)
+        {
+            for(int col=0;col<numcols;col++)
+            {
+               System.out.print(matrix[row][col]+"\t");;
+            }
+            System.out.println();
+        }
+        }
+        
+}
